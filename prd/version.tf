e438d7e02aa8367550a6aae9eeb04b5a8274fe45
Naveen Mummadi <113600766+naveenbabu01@users.noreply.github.com>
2024-02-26 05:20:51 +0530

    Create version.tf

---
+++ prd/version.tf
@@ -0,0 +1,25 @@
+terraform {
+
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "storagerg"
+    storage_account_name = "backendstore1206"
+    container_name       = "storecont"
+    key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "dd841e0c-a76f-4d5d-ae2f-b0645b248ae5"
+  tenant_id       = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
+}
