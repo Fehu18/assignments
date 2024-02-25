8f9898fae5280521374f6c447df260342b12cfc4
Naveen Mummadi <113600766+naveenbabu01@users.noreply.github.com>
2024-02-25 19:36:24 +0530

    Update main.tf

--- prd/main.tf
+++ prd/main.tf
@@ -1,3 +1,30 @@
+
+terraform {
+
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "storagerg"
+    storage_account_name = "backendstore1206"
+    container_name       = "storecont"
+    key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "dd841e0c-a76f-4d5d-ae2f-b0645b248ae5"
+  tenant_id       = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
+}
+
 module "resource_group" {
   source                  = "../modules/resource_group"
   resource_group_name     = "rg_eastus"
