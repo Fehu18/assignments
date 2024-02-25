b164cc01bcad630cc163c65dd15bfb0593922573
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-25 17:00:38 +0530

    Delete Terraform directory

--- Terraform/modules/KV/kv.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_key_vault""kv1"{
-    name = "var.name"
-    location = ""
-    resource_group_name = ""
-    sku_name = "var.sku_name"
-    tenant_id = ""
-}
--- Terraform/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-Rg_name = "rg1"
-location = "eastus"
-vnet_name = "net1"
-Vm_name   = "vm1"
-
