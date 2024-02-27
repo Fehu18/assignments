56a579492fe1543b05c9658fe13eb8788c82544d
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-27 17:36:07 +0530

    Update main.tf
    create a secret with vmpassword

--- modules/secret/main.tf
+++ modules/secret/main.tf
@@ -1,7 +1,12 @@
-
-resource "azurerm_key_vault_secret" "secret" {
-  name         = "secret-sauce"
-  value        = "szechuan"
-  key_vault_id = var.key_vault_id
-}
+resource "random_passwword" "vmpassword"{
+  length = 20
+  special = true
+  }
+  
+resource "azurerm_key_vault_secret""vmpassword"{
+   name = "vmpassword"
+   value = random_password.vmpassword.result
+   key_vault_id = var.key_vault_id
+   depends_on = [ azurerm_key_vault.kv]
+   }
 
