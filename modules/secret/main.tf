b48e52635b0ec5da137a15be1d5f14f1909cac8b
raihaniqbal1999 <raihaniqbal@kpmg.com>
2024-02-27 15:13:36 +0530

    added variable

--- modules/secret/main.tf
+++ modules/secret/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_secret" "secret" {
   name         = "secret-sauce"
   value        = "szechuan"
-  key_vault_id = module.keyvault.id
+  key_vault_id = var.key_vault_id
 }
 
