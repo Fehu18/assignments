8ed58a8c327e0343585513e05179d976313c6894
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-27 15:50:00 +0530

    Update version.tf
    updated object id details

--- prd/version.tf
+++ prd/version.tf
@@ -22,4 +22,5 @@ provider "azurerm" {
   features {}
   subscription_id = "dd841e0c-a76f-4d5d-ae2f-b0645b248ae5"
   tenant_id       = "6a7cad51-05b4-4ea3-8435-b2157749ac6b"
+  object_id       = "4bbcfb7b-8415-4828-aee5-c19aa18500d4"
 }
