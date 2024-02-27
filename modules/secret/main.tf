36df07a9fee8f01f432836eca4fff4ef468447f7
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-27 21:38:57 +0530

    Update main.tf
    changes aded

--- modules/secret/main.tf
+++ modules/secret/main.tf
@@ -1,12 +1,17 @@
-resource "random_password" "vmpassword"{
-  length = 20
+variable "key_vault_id" {
+  description = "The ID of the Key Vault where the secret will be stored"
+  type        = string
+}
+
+resource "random_password" "vmpassword" {
+  length  = 20
   special = true
-  }
-  
-resource "azurerm_key_vault_secret""vmpassword"{
-   name = "vmpassword"
-   value = random_password.vmpassword.result
-   key_vault_id = var.key_vault_id
-   depends_on = [module.kv]
-   }
+}
+
+resource "azurerm_key_vault_secret" "vmpassword" {
+  name         = "vmpassword"
+  value        = random_password.vmpassword.result
+  key_vault_id = var.key_vault_id
+}
+
 
