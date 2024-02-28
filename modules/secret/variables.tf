ae604436c9bc7ac2df81121cf1de250cea77d09f
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-29 01:08:28 +0530

    Update variables.tf

--- modules/secret/variables.tf
+++ modules/secret/variables.tf
@@ -1,6 +1,5 @@
 variable "key_vault_id" {
-  description = "Azure Key Vault containing TLS certificates (will also be used to store Vault seal secret)"
-  type        = string
+  default     = ""
 }
 
 
