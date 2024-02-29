3aa2b72b023168fd5f33a41c965834845431904f
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-29 09:02:09 +0530

    Update main.tf

--- modules/secret/main.tf
+++ modules/secret/main.tf
@@ -1,12 +1,8 @@
-resource "random_password" "vmpassword" {
-  length  = 20
-  special = true
-}
-
 resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
-  value        = random_password.vmpassword.result
-  key_vault_id = var.key_vault_id
+  value        = "Azure@123"
+   key_vault_id = var.key_vault_id
 }
+  
 
 
