fab3c59e3495e33884fd4fe2fdf81cc491e85b2e
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-29 09:47:20 +0530

    Update main.tf

--- modules/kv/main.tf
+++ modules/kv/main.tf
@@ -20,17 +20,5 @@ resource "azurerm_key_vault" "main" {
   }
 }
 
-resource "azurerm_key_vault_access_policy" "example" {
-  key_vault_id =  var.key_vault_id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "delete",
-    "get",
-    "set",
-  ]
-}
 
 
