a8bd472bfc77b18f293bd11039b70dc3aa86bf08
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-29 09:38:30 +0530

    Update main.tf

--- modules/secret/main.tf
+++ modules/secret/main.tf
@@ -2,6 +2,8 @@ resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
   value        = "Azure@123"
    key_vault_id = var.key_vault_id
+
+ depends_on = [azurerm_key_vault_access_policy.example]
 }
   
 
